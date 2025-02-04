912860eef44bfbc36ba44374a9a4882040f0a143
Geetansh Jain <gjain@covalience.int>
2025-02-04 14:58:55 +0530

    done

---
+++ JS/script.js
@@ -0,0 +1,97 @@
+var resume_pause = document.querySelector("#resume-pause");
+var start = document.querySelector("#start");
+var showTime = document.querySelector("#showTime");
+var reset = document.querySelector("#reset");
+var progressBar = document.querySelector("#progress");
+var globalTime = 0
+var minTime = document.querySelectorAll(".time-btn")
+
+minTime.forEach((e) => {
+    e.addEventListener("click", () => {
+        globalTime = e.value
+        displayTime(globalTime, 0)
+    })
+})
+
+var pauseInterval = null;
+var timeInterval = null;
+var timeLeft = 0;
+var totalTime = 0;
+
+start.addEventListener("click", () => {
+    if (showTime.textContent !== "00:00") {
+        let startAudio = new Audio("./AUDIOS/task_start.wav");
+        startAudio.play();
+        displayTime(globalTime, 1)
+
+    }
+
+    else {
+        alert("select time");
+    }
+});
+
+resume_pause.addEventListener("click", () => {
+    if (showTime.textContent !== "00:00") {
+        if (resume_pause.textContent === "Pause") {
+            resume_pause.textContent = "Resume";
+            let alarmAudio = new Audio("./AUDIOS/task_alarm.wav");
+            pauseInterval = setInterval(() => {
+                alarmAudio.play();
+            }, 500);
+            clearInterval(timeInterval);
+        }
+
+        else {
+            resume_pause.textContent = "Pause";
+            clearInterval(pauseInterval);
+            timeInterval = setInterval(updateTime, 1000);
+        }
+    }
+
+    else {
+        alert("time not started");
+    }
+});
+
+function resetTask() {
+    let resetAudio = new Audio("./AUDIOS/task_reset.wav");
+    resetAudio.play()
+    showTime.textContent = "00:00";
+    timeLeft = 0;
+    totalTime = 1;
+    updateDisplay()
+    clearInterval(timeInterval)
+    clearInterval(pauseInterval)
+
+}
+
+function displayTime(min, check) {
+    resetTask();
+    timeLeft = min * 60;
+    totalTime = min * 60;
+    updateDisplay();
+
+    if (check == 1) timeInterval = setInterval(updateTime, 1000);
+}
+
+function updateTime() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        updateDisplay();
+    }
+
+    else {
+        let taskFinish = new Audio("./AUDIOS/task_finish.wav");
+        taskFinish.play();
+        resetTask()
+    }
+}
+
+function updateDisplay() {
+    let minLeft = Math.floor(timeLeft / 60);
+    let secLeft = timeLeft % 60;
+    showTime.textContent = `${minLeft < 10 ? '0'+minLeft :minLeft}:${secLeft < 10 ? '0'+secLeft :secLeft}`;
+    let width = (timeLeft / totalTime) * 100
+    progressBar.style.width = `${width}%`
+}
